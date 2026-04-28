ff3d917503a0dd27191ead4535a5ef75ef10f65c
Nike <2413027@students.universitasmulia.ac.id>
2026-04-28 22:51:36 +0800

    upload pertama

---
+++ app/abc/page.tsx
@@ -0,0 +1,8 @@
+export default function ABC() {
+    return (
+        <div>
+            <p>halaman abc. meh</p>
+            <input type="teks" placeholder="masukkan teks"></input>
+        </div>
+    );
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function BLOG() {
+    return (
+        <div>
+            <p>ini halaman blog</p>
+            <input type="text" placeholder="masukkan teks disini"></input>
+        </div>
+    );
+}
