2eedbe3df1828f7dfcd1bb98a13a043ae51af871
Nike <2413027@students.universitasmulia.ac.id>
2026-04-29 19:16:04 +0800

    Tugas1

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,7 @@
 export default function BLOG() {
     return (
         <div>
-            <p>ini halaman blog</p>
+            <p>ini halaman Blog!</p>
             <input type="text" placeholder="masukkan teks disini"></input>
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            nike
+            Avril
           </a>
         </div>
       </main>
