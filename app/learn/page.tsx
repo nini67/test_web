9d0562c71af2ee25d1d7f6898b3e38078c2c2a32
Nike <2413027@students.universitasmulia.ac.id>
2026-05-03 23:37:04 +0800

    update layout

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,14 +1,66 @@
 export default function LearnPage() {
     return (
-        <div>
-            <div>
-                <img/>
-                <p>Design</p>
+        <div className="bg-white h-screen flex items-center justify-center">
+        <div className="flex flex-col gap-4">
+            <div className="flex gap-4">
+                <div className="bg-blue-300 h-72 w-96 p-8 flex flex-col justify-between rounded-4xl">
+                <div className="flex gap-4">
+                    <img src={"/palette.svg"} className="w-6 h-6 invert" alt="Icon palette" />
+                    <p>Design</p>
+                </div>
+                <div>
+                    <h1 className="text-4xl font-bold">Adobe Photoshop</h1>
+                    <p>In 3 days</p> 
+                </div>
+                </div>
+
+                <div className="bg-green-300 h-72 w-106 p-8 flex flex-col justify-between rounded-4xl">
+                <div className="flex gap-4">
+                    <img src={"/AI.svg"} className="w-6 h-6 invert" alt="Icon AI" />
+                    <p>AI</p>
+                </div>
+                <div>
+                    <h1 className="text-4xl font-bold">DALL.E 2, Midjourney, Stable Diffusion</h1>
+                    <p>In 5 days</p> 
+                </div>
+                </div>
             </div>
-            <div>
-                <h1>Figma</h1>
-                <p>8 hours ago</p>
-            </div>
+
+            <div className="flex gap-4">
+                <div className="bg-purple-300 h-62 w-66 p-8 flex flex-col justify-between rounded-4xl">
+                <div className="flex gap-4">
+                    <img src={"/palette.svg"} className="w-6 h-6 invert" alt="Icon Palette" />
+                    <p>Design</p>
+                </div>
+                <div>
+                    <h1 className="text-4xl font-bold">Figma</h1>
+                    <p>8 hours ago</p>
+                </div>
+                </div>
+
+                <div className="bg-orange-300 h-62 w-66 p-8 flex flex-col justify-between rounded-4xl">
+                <div className="flex gap-4">
+                    <img src={"/codding.svg"} className="w-6 h-6 invert" alt="Icon Codding"/>
+                    <p>Codding</p>
+                </div>
+                <div>
+                    <h1 className="text-4xl font-bold">Pyhton</h1>
+                    <p>2 days ago</p>
+                </div>
+                </div>
+            
+                <div className="bg-red-300 h-62 w-66 p-8 flex flex-col justify-between rounded-4xl">
+                <div className="flex gap-4">
+                    <img src={"/palette.svg"} className="w-6 h-6 invert" alt="Icon Palette" />
+                    <p>Design</p>
+                </div>
+                <div>
+                    <h1 className="text-4xl font-bold">Sketch</h1>
+                    <p>4 days ago</p>
+                </div>
+                </div>
+            </div>    
+        </div>    
         </div>
     );
 }
