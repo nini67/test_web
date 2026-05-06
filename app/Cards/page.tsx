52b7b690082a97919ba93399da1298e39335d3ee
Nike <2413027@students.universitasmulia.ac.id>
2026-05-06 21:51:58 +0800

    update Cards

---
+++ app/Cards/page.tsx
@@ -0,0 +1,18 @@
+export default function Cards() {
+    return (
+        <div className="bg-gray h-screen flex items-center justify-center">
+        <div className="container">
+            <div className="card blue">
+                <div className="bg-white h-72 w-70 p-8 flex flex-col justify-between rounded-4xl">
+                    <div className="bg-blue-200 h-45 w-55 p-6 justify-between rounded-4xl" >
+
+                    </div>
+                    <div className="bg-gray-100 h-10 w55 p-5 justify-between rounded-4xl ">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+}
